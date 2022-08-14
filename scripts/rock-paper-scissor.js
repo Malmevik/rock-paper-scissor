7defe7d8a2785e3702fe94eab4e06144f7bf3882
Malmevik <jonas.malmevik@gmail.com>
2022-08-14 21:20:03 +0200

    Added game, storeScore and determineWinner functions

--- scripts/rock-paper-scissor.js
+++ scripts/rock-paper-scissor.js
@@ -1,4 +1,5 @@
-
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice()
 {
@@ -35,4 +36,45 @@ function playRound(playerChoice)
             break;
     } 
     console.log(winText);
+    return winner;
 }
+
+
+function game(){
+    let playerChoice;
+    let winner;
+
+    for (let rounds = 0; rounds < 5; rounds++) {
+        let accepted = false;
+
+        while(!accepted)
+        {
+            playerChoice = prompt("Rock, Paper, Scissor?")
+            if(playerChoice.toLowerCase() === "rock" || playerChoice.toLowerCase() === "scissor" || playerChoice.toLowerCase() === "paper")
+                accepted = true;
+            else
+                alert("Must input rock paper or scissor!");
+        }
+        storeScore(playRound(playerChoice));
+    }
+
+    determineWinner();
+}
+
+function storeScore(winner)
+{
+    if(winner === 0)
+        playerScore++;
+    else if(winner === 1)
+        computerScore++;
+}
+
+function determineWinner()
+{
+    if(playerScore > computerScore)
+        console.log(`You won with ${playerScore} against ${computerScore}!`)
+    else if(playerScore < computerScore)
+        console.log(`You lost with ${playerScore} against ${computerScore}!`)
+    else 
+        console.log(`It's a draw with ${playerScore} against ${computerScore}!`)
+}   
