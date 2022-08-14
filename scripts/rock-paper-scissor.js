4fccce5ca9105db74f4f4620ee6ed225fd81197a
Malmevik <jonas.malmevik@gmail.com>
2022-08-14 20:51:56 +0200

    Added playRound function

--- scripts/rock-paper-scissor.js
+++ scripts/rock-paper-scissor.js
@@ -3,5 +3,41 @@
 function getComputerChoice()
 {
     let choice = Math.floor(Math.random() * 3) 
-    return choice === 0 ? "Rock" : choice === 1 ? "Paper" : choice === 2 ? "Scissor" : null
+    return choice === 0 ? "rock" : choice === 1 ? "paper" : choice === 2 ? "scissor" : null
+}
+
+function playRound(playerChoice)
+{
+    const computerChoice = getComputerChoice();
+    let winner;
+    let winText;
+    
+    if(playerChoice.toLowerCase() == "rock" && computerChoice == "scissor")
+    {
+        winner = 0; 
+    } else if(playerChoice.toLowerCase() == "paper" && computerChoice == "rock")
+    {
+        winner = 0;
+    } else if(playerChoice.toLowerCase() == "paper" && computerChoice == "rock")
+    {
+        winner = 0;
+    } else if(playerChoice.toLowerCase() === computerChoice)
+    {
+        winner = 2;
+    }
+    else winner = 1;
+
+    switch(winner)
+    {
+        case 0 :
+            winText = "You won! " + playerChoice.toLowerCase() + " beats "  + computerChoice + "!";
+            break;
+        case 1 :
+            winText = "You lost! " + computerChoice + " beats "  + playerChoice.toLowerCase() + "!";
+            break;
+        case 2 :
+            winText = "It's a draw! You both chose: " + computerChoice + "!";  
+            break;
+    } 
+    console.log(winText);
 }
